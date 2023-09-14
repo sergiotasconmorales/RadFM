cc38fee3cbba3e809a1f2925de40d45ed280ddd4
Sergio Tascon Morales <s.tasconmorales@gmail.com>
2023-09-14 14:03:04 +0200

    Change parallel init

--- Quick_demo/test.py
+++ Quick_demo/test.py
@@ -4,6 +4,8 @@ from typing import Optional, Dict, Sequence
 from typing import List, Optional, Tuple, Union
 import transformers
 import torch.distributed as dist
+import torch.backends.cudnn as cudnn
+import numpy as np
 from dataclasses import dataclass, field
 from Model.RadFM.multimodality_model import MultiLLaMAForCausalLM
 import torch
@@ -11,6 +13,78 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaTokenizer
 from torchvision import transforms
 from PIL import Image   
 from torch.nn.parallel import DistributedDataParallel as DDP
+import util.misc as misc
+import argparse
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('llama_adapterV2 pre-training', add_help=False)
+    parser.add_argument('--batch_size', default=64, type=int,
+                        help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
+    parser.add_argument('--epochs', default=400, type=int)
+    parser.add_argument('--accum_iter', default=1, type=int,
+                        help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
+    parser.add_argument('--save_every_epochs', default=5, type=int,
+                        help='save model every N epochs')
+
+
+    # Model parameters
+    parser.add_argument('--llama_type', default='7B', type=str,
+                        help='Type of LLaMA model') #
+    parser.add_argument('--llama_path', default='/path/to/llama', type=str,
+                        help='path to LLaMA pretrained checkpoint')
+    parser.add_argument('--max_words', default=96, type=int,
+                        help='max number of input words')
+    parser.add_argument('--clip_model', default='ViT-L/14', type=str,
+                        help='CLIP model name')
+    parser.add_argument('-train_clip_visual', action='store_true', help='train clip visual')
+
+    # Optimizer parameters
+    parser.add_argument('--weight_decay', type=float, default=0.05,
+                        help='weight decay (default: 0.05)')
+
+    parser.add_argument('--lr', type=float, default=None, metavar='LR',
+                        help='learning rate (absolute lr)')
+    parser.add_argument('--blr', type=float, default=1e-3, metavar='LR',
+                        help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
+    parser.add_argument('--min_lr', type=float, default=0., metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0')
+
+    parser.add_argument('--warmup_epochs', type=int, default=40, metavar='N',
+                        help='epochs to warmup LR')
+
+    # Dataset parameters
+    parser.add_argument('--data_config', default='configs/data/pretrain/EN.yaml', type=str,
+                        help='dataset config path')
+    parser.add_argument('--num_workers', default=10, type=int)
+    parser.add_argument('--pin_mem', action='store_true',
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
+    parser.set_defaults(pin_mem=True)
+
+
+    parser.add_argument('--output_dir', default='./output',
+                        help='path where to save, empty for no saving')
+    parser.add_argument('--log_dir', default='./output',
+                        help='path where to tensorboard log')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=0, type=int)
+
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', action='store_true')
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
+
+    parser.add_argument('--split_epoch', type=int, default=50)
+
+    return parser
 
 def get_tokenizer(tokenizer_path, max_img_size = 100, image_num = 32):
     '''
@@ -76,15 +150,25 @@ def combine_and_preprocess(question,image_list,image_padding_tokens):
     return text, vision_x, 
     
     
-def main():
+def main(args):
     
+    # Paralellism init from openclip
+    misc.init_distributed_mode(args)
+    device = torch.device(args.device)
+
+    # fix the seed for reproducibility
+    seed = args.seed + misc.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    cudnn.benchmark = True
+
     # set up parallel shit
-    n_gpus = torch.cuda.device_count()
-    world_size = n_gpus
-    rank = dist.get_rank()
-    dist.init_process_group("nccl")
-    print(f"Start running DDP on rank {rank}.")
-    device_id = rank % world_size
+    #n_gpus = torch.cuda.device_count()
+    #world_size = n_gpus
+    #rank = dist.get_rank()
+    #dist.init_process_group("nccl")
+    #print(f"Start running DDP on rank {rank}.")
+    #device_id = rank % world_size
 
     print("Setup tokenizer")
     text_tokenizer,image_padding_tokens = get_tokenizer('./Quick_demo/Language_files')
@@ -107,8 +191,8 @@ def main():
     print("Setup Model")
     model = MultiLLaMAForCausalLM(
         lang_model_path='./Quick_demo/Language_files', ### Build up model based on LLaMa-13B config
-    )
-    model = DDP(model, device_ids=[rank])
+    ).to(device)
+    model = DDP(model, device_ids=[args.gpu], find_unused_parameters=True)
     #fsdp_model = FullyShardedDataParallel(
     """
     ckpt = torch.load('./Quick_demo/pytorch_model.bin', map_location ='cpu') # Please dowloud our checkpoint from huggingface and Decompress the original zip file first
@@ -132,5 +216,7 @@ def main():
     """
     
 if __name__ == "__main__":
-    main()
+    args = get_args_parser()
+    args = args.parse_args()
+    main(args)
        
